0829ae008e15d5f3f2a4b12c9a78d7c8b7099418
Artyom Krasyuk <artem.krasyuk2004@gmail.com>
2024-06-22 12:51:47 +0400

    version 3

--- authorization/src/main/java/com/example/authorization/models/User.java
+++ authorization/src/main/java/com/example/authorization/models/User.java
@@ -4,10 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
 import java.util.UUID;
 
 @Getter
@@ -16,12 +14,13 @@ import java.util.UUID;
 @NoArgsConstructor
 @Entity
 @Table(name="users")
-public class UserEntity {
+public class User {
     @Id
     private UUID id;
     private String username;
     @Column(unique = true)
     private String phone;
+    @Column(unique = true)
     private String email;
     private String password;
 }
--- authorization/src/main/java/com/example/authorization/repos/UserRepository.java
+++ authorization/src/main/java/com/example/authorization/repos/UserRepository.java
@@ -1,10 +1,15 @@
 package com.example.authorization.repos;
 
 
-import com.example.authorization.models.UserEntity;
+import com.example.authorization.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends CrudRepository<UserEntity, UUID> {
+@Repository
+public interface UserRepository extends CrudRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
